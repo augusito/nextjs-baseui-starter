42a92fe0f6f35f3c9cc3d1a868f443e1b25b2d4e
augusito <kaugustus15@gmail.com>
2022-12-09 18:59:15 +0300

    refactor: home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import App from "next/app";
 import type { AppProps } from "next/app";
 import { Provider as StyletronProvider } from "styletron-react";
-import { LightTheme, BaseProvider } from "baseui";
+import { DarkTheme, BaseProvider } from "baseui";
 import { styletron } from "../styletron";
 
 export default class MyApp extends App {
@@ -14,7 +14,7 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <StyletronProvider value={styletron}>
-        <BaseProvider theme={LightTheme}>
+        <BaseProvider theme={DarkTheme}>
           <Component {...pageProps} />
         </BaseProvider>
       </StyletronProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import * as React from "react";
-import { Button } from "baseui/button";
-import { useStyletron } from "baseui";
+import { Block } from "baseui/block";
 
 export const sum = (a: number, b: number) => a + b;
 
 export default function Home() {
-  const [css, theme] = useStyletron();
   return (
     <>
-      <Button onClick={() => console.log("hey")}>Hello</Button>
-      <p className={css({ color: theme.colors.accent600 })}>Styled by hook</p>
+      <Block backgroundColor="backgroundSecondary" color={"contentPrimary"}>
+        Hello Base Web!
+      </Block>
     </>
   );
 }
